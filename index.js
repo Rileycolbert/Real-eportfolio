fbecdff34c30ffda32217506d65d6c08080dea09
Riley.colbert <levighjay@gmail.com>
2022-07-25 10:51:06 +1000

    modal form completed

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 
 function contact(event) {
     event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading');
+const success = document.querySelector('.modal__overlay--success');
+loading.classList += " modal__overlay--visible"
 emailjs
 .sendForm(
     'service_mxqbf8l', 
@@ -10,7 +13,12 @@ emailjs
     event.target,
     '9Tyq8RczADoQz7FUY'
 ).then(() => {
-    console.log('this worked1')
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible"
+}).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+        "The email service is temporarily unavailable. Please contact me directly on codingriley@gmail.com"
+    );
 })
 }
-
