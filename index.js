2ca5be57742c177b320d2508c3ab7fb799e95a65
Riley.colbert <levighjay@gmail.com>
2022-07-25 11:23:06 +1000

    modal transitions added

--- index.js
+++ index.js
@@ -22,3 +22,13 @@ emailjs
     );
 })
 }
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
