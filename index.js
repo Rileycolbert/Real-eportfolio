d1dbef592ecc67e4f2c0803417d8a70c70272c90
Riley.colbert <levighjay@gmail.com>
2022-07-31 22:55:14 +1000

    Toggle contrast styling

--- index.js
+++ index.js
@@ -1,74 +1,12 @@
-// let isModalOpen = false;
-// let contrastToggle = false;
-
-// function toggleContrast() {
-//   contrastToggle = !contrastToggle;
-//   if (contrastToggle) {
-//     document.body.classList += " dark-theme";
-//   } else {
-//     document.body.classList.remove("dark-theme");
-//   }
-// }
-
-// function contact(event) {
-//   event.preventDefault();
-//   const loading = document.querySelector(".modal__overlay--loading");
-//   const success = document.querySelector(".modal__overlay--success");
-//   loading.classList += " modal__overlay--visible";
-//   emailjs
-//     .sendForm(
-//       "service_mxqbf8l",
-//       "template_ls1s9q9",
-//       event.target,
-//       "9Tyq8RczADoQz7FUY"
-//     )
-//     .then(() => {
-//       loading.classList.remove("modal__overlay--visible");
-//       success.classList += " modal__overlay--visible";
-//     })
-//     .catch(() => {
-//       loading.classList.remove("modal__overlay--visible");
-//       alert(
-//         "The email service is temporarily unavailable. Please contact me directly on codingriley@gmail.com"
-//       );
-//     });
-// }
-
-
-// function toggleModal() {
-//   if (isModalOpen) {
-//     isModalOpen = false;
-//     return document.body.classList.remove("modal--open");
-//   }
-//   isModalOpen = true;
-//   document.body.classList += " modal--open";
-// }
-
-
 let isModalOpen = false;
 let contrastToggle = false;
-const scaleFactor = 1 / 20;
-
-function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape");
-  const x = event.clientX * scaleFactor;
-  const y = event.clientY * scaleFactor;
-
-  for (let i = 0; i < shapes.length; ++i) {
-    const isOdd = i % 2 !== 0;
-    const boolInt = isOdd ? -1 : 1;
-    // Added rotate after tutorial
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
-  }
-}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
   if (contrastToggle) {
-    document.body.classList += " dark-theme"
-  }
-  else {
-    document.body.classList.remove("dark-theme")
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
   }
 }
 
@@ -79,10 +17,10 @@ function contact(event) {
   loading.classList += " modal__overlay--visible";
   emailjs
     .sendForm(
-      "service_80ih0if",
-      "template_d9refyl",
+      "service_mxqbf8l",
+      "template_ls1s9q9",
       event.target,
-      "user_K1PoFs8pB2YVWStDxrUls"
+      "9Tyq8RczADoQz7FUY"
     )
     .then(() => {
       loading.classList.remove("modal__overlay--visible");
@@ -91,10 +29,11 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly on codingriley@gmail.com"
       );
     });
 }
+
 
 function toggleModal() {
   if (isModalOpen) {
